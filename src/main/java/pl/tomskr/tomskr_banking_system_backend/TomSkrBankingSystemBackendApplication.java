cf0b853a5917f1c687dfeff23835e1909843787a
TomaszSkrzypek <tomskr3.ts@gmail.com>
2024-07-04 22:04:44 +0200

    Initial Commit

---
+++ src/main/java/pl/tomskr/tomskr_banking_system_backend/TomSkrBankingSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.tomskr.tomskr_banking_system_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomSkrBankingSystemBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TomSkrBankingSystemBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/tomskr/tomskr_banking_system_backend/TomSkrBankingSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.tomskr.tomskr_banking_system_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TomSkrBankingSystemBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
