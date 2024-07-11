11dfcccac662c1970246b39763a69d46f6c3a82a
tomskr <33270937+tomskr@users.noreply.github.com>
2024-07-11 08:39:58 +0000

    Create account service

--- src/main/java/pl/tomskr/tomskr_banking_system_backend/Services/AccountServices.java
+++ src/main/java/pl/tomskr/tomskr_banking_system_backend/Services/AccountServices.java
@@ -5,7 +5,16 @@ import org.springframework.stereotype.Service;
 @Service
 public class AccountServices {
 
+    private final AccountRepository accountRepository;
+
+    AccountServices(AccountRepository accountRepository){
+        this.accountRepository = accountRepository;
+    }
+
     //todo create account
+     public Account createAccount(Account account){
+        return accountRepository.save(account);
+     }
 
     //todo display account
 
