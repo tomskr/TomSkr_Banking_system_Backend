c2fa948fcae121a6bb05c869d3c7fc17dff7725e
TomaszSkrzypek <tomskr3.ts@gmail.com>
2024-07-14 17:30:27 +0200

    Controller added deposit and withdraw method

--- src/main/java/pl/tomskr/tomskr_banking_system_backend/Controller/BankingController.java
+++ src/main/java/pl/tomskr/tomskr_banking_system_backend/Controller/BankingController.java
@@ -2,14 +2,14 @@ package pl.tomskr.tomskr_banking_system_backend.Controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.tomskr.tomskr_banking_system_backend.Domain.Account;
 import pl.tomskr.tomskr_banking_system_backend.Services.AccountServices;
 
+import java.util.Map;
+
 @RestController
+@RequestMapping("/api/accounts")
 public class BankingController {
     private final AccountServices accountServices;
 
@@ -17,16 +17,30 @@ public class BankingController {
         this.accountServices = accountServices;
     }
 
-    //todo: Display account
 
-    //todo: Create account
+    @GetMapping("/{id}")
+    public Account getAccount(@PathVariable Long id){
+        return accountServices.getAccount(id).orElseThrow(()-> new RuntimeException("Account not Found"));
+    }
+
+
     @PostMapping
     public ResponseEntity<Account> createAccount(@RequestBody Account newAccount){
         Account saveAccount = accountServices.createAccount(newAccount);
         return ResponseEntity.status(HttpStatus.CREATED).body(saveAccount);
     }
-    //todo: Make Deposit
 
-    //todo: Make withdraw
+
+    @PostMapping("/{id}/deposit")
+    public Account deposit(@PathVariable Long id, @RequestBody Map<String, Double> request) {
+        Double amount = request.get("amount");
+        return accountServices.makeDeposit(amount, id);
+    }
+
+    @PostMapping("/{id}/withdraw")
+    public Account withdraw(@PathVariable Long id, @RequestBody Map<String, Double> request){
+        Double amount = request.get("amount");
+        return accountServices.makeWithdraw(amount, id);
+    }
 
 }
